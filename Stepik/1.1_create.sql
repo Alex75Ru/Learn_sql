820e542448bd10394613669bf5ec40bac5c60c10
Alex75Ru <74034395+Alex75Ru@users.noreply.github.com>
2020-11-06 13:02:36 +0900

    Update 1.1_create.sql

--- Stepik/1.1_create.sql
+++ Stepik/1.1_create.sql
@@ -1,5 +1,7 @@
 CREATE TABLE book(book_id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(50), author VARCHAR (30), price DECIMAL (8,2), amount INT);
 /* 
+SQL Server вместо AUTO_INCREMENT используйте IDENTITY
+PostgreSQL вместо INT AUTO_INCREMENT используйте SERIAL
   ключевые слова : CREATE TABLE
 имя создаваемой таблицы;
 открывающая круглая скобка «(»;
